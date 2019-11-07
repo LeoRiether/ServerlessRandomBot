b240befe7a246163f5db2ea5d5319ae9147898c1
Leonardo Riether <leonardoalves8217@gmail.com>
2019-11-07 10:28:02 -0300

    1st commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ServerlessRandomBot
+Created in less than 40 minutes to choose who would get the best bed in AirBnb
---
+++ api/dice.go
@@ -0,0 +1,10 @@
+package api
+
+import (
+	"net/http"
+)
+
+// Dice responds to /api/dice with a random number between [0,4)
+func Dice(w http.ResponseWriter, r *http.Request) {
+
+}
