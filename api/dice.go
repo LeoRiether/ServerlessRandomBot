d7b0339c3c94d55f218a3bd09a0157a2ba90b2af
Leonardo Riether <leonardoalves8217@gmail.com>
2019-11-07 10:44:58 -0300

    gogogo

--- api/dice.go
+++ api/dice.go
@@ -1,10 +1,32 @@
 package api
 
 import (
+	"encoding/json"
+	"math/rand"
 	"net/http"
 )
 
+type Response struct {
+	ChatID int    `json:"chat_id"`
+	Text   string `json:"text"`
+}
+
 // Dice responds to /api/dice with a random number between [0,4)
 func Dice(w http.ResponseWriter, r *http.Request) {
+	if r.Body == nil {
+		return
+	}
 
+	defer r.Body.Close()
+	decoder := json.NewDecoder(r.Body)
+	inp := make(map[string]interface{})
+	decoder.Decode(&inp)
+
+	response := Response{
+		ChatID: int(inp["message"]["chat"]["id"]),
+		Text: fmt.Sprintf("Rolled a %d", rand.Intn(4)),
+	}
+
+	out, _ := json.Marshal(response)
+	w.Write(out)
 }
